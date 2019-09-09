b96b62786d1fe5c82e162a9ac0bcd69fc9a36b99
Artem B <arty.boiko@gmail.com>
2019-09-09 18:42:57 +0300

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Artem Boiko on 9/9/19.
+//  Copyright © 2019 Artem Boiko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
